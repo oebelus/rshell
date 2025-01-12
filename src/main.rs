4ac3ad173bedfe1537fecf3ce865ee3a9e513f33
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2025-01-12 23:07:39 +0100

    executables

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process::exit;
+use std::{env, process::exit};
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
 
-    let path = match args.get(1) {
-        Some(path) => path,
-        None => "",
+    let path = match env::var("PATH") {
+        Ok(p) => p,
+        Err(_) => String::new(),
     };
 
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        // Wait for user input
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
@@ -24,13 +22,19 @@ fn main() {
 }
 
 fn handle_input(input: &str, path: &str) {
-    // let builtins= ["exit", "echo", "type"];
+    let builtins= ["exit", "echo", "type"];
 
     match input.trim() {
         input if input.starts_with("echo") => println!("{}", input[5..].trim()),
         input if input.starts_with("type") => {
             let command = input[5..].trim();
-            executable_exists(path, command);
+            if builtins.contains(&command) {
+                println!("{} is a shell builtin", command);
+                return
+            }
+            else { 
+                executable_exists(path, command);
+            }
         }
         "exit 0" => exit(0),
         _ => println!("{}: command not found", input.trim())
@@ -44,8 +48,9 @@ fn executable_exists(path: &str, command: &str) {
         let full_path = format!("{}/{}", directory, command);
         if std::fs::metadata(&full_path).is_ok() {
             println!("{} is {}", command, full_path);
+            return;
         }
     }
 
-    println!("{}: command not found", command)
+    println!("{}: not found", command)
 }
